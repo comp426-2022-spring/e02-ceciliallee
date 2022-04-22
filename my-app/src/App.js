e2fb7adb384c2ea366f7dd1e16bca17398f00dd3
Cecilia (Chae Hyun) Lee <chaelee@email.unc.edu>
2022-04-21 20:40:31 -0400

    error change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -56,8 +56,7 @@ function App() {
     if(currentSum==='')
     return;
 // 2. This line is where you add the points all together. Add your other variable references here. Make sure that they match what is above and what is below in the HTML form
-    let sum = parseInt(currentA00) + parseInt(currentA01) + parseInt(currentA02) + parseInt(currentA03) + parseInt(currentA04) + parseInt(currentA05)
-    parseInt(currentE01) + parseInt(currentE02) + parseInt(currentCommits) + parseInt(currentMidterm) + parseInt(currentProject)
+    let sum = parseInt(currentA00) + parseInt(currentA01) + parseInt(currentA02) + parseInt(currentA03) + parseInt(currentA04) + parseInt(currentA05) + parseInt(currentE01) + parseInt(currentE02) + parseInt(currentCommits) + parseInt(currentMidterm) + parseInt(currentProject)
       + parseInt(currentQ01) + parseInt(currentQ02) + parseInt(currentQ03);
     setCurrentSum(sum);
   }
